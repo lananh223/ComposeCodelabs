3535f9912903bdaf3402cdc26f09efae63d369d4
Lan <lan.anh.7988@gmail.com>
2023-09-13 17:55:16 -0700

    Add Column, row, elevated button to the UI

--- app/src/main/java/com/example/composecodelabs/MainActivity.kt
+++ app/src/main/java/com/example/composecodelabs/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.composecodelabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -28,28 +31,43 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     // Surface is setting background color
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(
-            text = "Hello $name!",
-            modifier = Modifier.padding(24.dp)
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeCodelabsTheme {
-        Greeting("Android")
-    }
-}
-
-@Composable
-private fun MyApp(modifier: Modifier = Modifier) {
     Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Greeting(name = "Android")
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+
+            ElevatedButton(onClick = { /* TODO */ }) {
+                Text("Show more")
+            }
+        }
+
+    }
+}
+
+//Dp = 320 is the common width of a small phone
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    ComposeCodelabsTheme {
+        MyApp()
+    }
+}
+
+@Composable
+private fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
