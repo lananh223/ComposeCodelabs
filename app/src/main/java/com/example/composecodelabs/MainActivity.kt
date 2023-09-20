fc4c503a976bf75413da068dbd9c740263bd7d52
Lan <lan.anh.7988@gmail.com>
2023-09-20 16:35:13 -0700

    Deal with configuration change

--- app/src/main/java/com/example/composecodelabs/MainActivity.kt
+++ app/src/main/java/com/example/composecodelabs/MainActivity.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -77,7 +78,7 @@ fun MyAppPreview() {
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
 
-    var shouldShowOnBoarding by remember {
+    var shouldShowOnBoarding by rememberSaveable {
         mutableStateOf(true)
     }
 
