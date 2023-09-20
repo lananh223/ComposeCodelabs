266602c4252b1b5ab21b07230290aa858dc24af9
Lan <lan.anh.7988@gmail.com>
2023-09-20 16:25:12 -0700

    Move state code to MyApp (the parent) - State hoisting

--- app/src/main/java/com/example/composecodelabs/MainActivity.kt
+++ app/src/main/java/com/example/composecodelabs/MainActivity.kt
@@ -3,17 +3,22 @@ package com.example.composecodelabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -61,14 +66,30 @@ fun Greeting(name: String) {
 //Dp = 320 is the common width of a small phone
 @Preview(showBackground = true, widthDp = 320)
 @Composable
-fun DefaultPreview() {
+fun MyAppPreview() {
     ComposeCodelabsTheme {
-        MyApp()
+        MyApp(Modifier.fillMaxSize())
     }
 }
 
 @Composable
-private fun MyApp(
+fun MyApp(modifier: Modifier = Modifier) {
+
+    var shouldShowOnBoarding by remember {
+        mutableStateOf(true)
+    }
+
+    Surface(modifier) {
+        if (shouldShowOnBoarding) {
+            OnBoardingScreen(onContinueClicked = { shouldShowOnBoarding = false })
+        } else {
+            Greetings()
+        }
+    }
+}
+
+@Composable
+private fun Greetings(
     modifier: Modifier = Modifier,
     names: List<String> = listOf("World", "Compose")
 ) {
@@ -76,5 +97,35 @@ private fun MyApp(
         for (name in names) {
             Greeting(name = name)
         }
+    }
+}
+
+@Composable
+private fun OnBoardingScreen(
+    onContinueClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Welcome to the Codelabs")
+        Button(
+            modifier = modifier.padding(vertical = 24.dp),
+            onClick = onContinueClicked
+        ) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+
+fun OnboardingPreview() {
+    ComposeCodelabsTheme {
+        OnBoardingScreen(onContinueClicked = {/* Do nothing */ })
     }
 }
