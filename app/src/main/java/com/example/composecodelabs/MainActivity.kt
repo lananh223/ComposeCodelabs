f57549458d12c2840e2b361b1ec7ae6f7aa439b9
Lan <lan.anh.7988@gmail.com>
2023-09-20 21:49:29 -0700

    Style and theming UI

--- app/src/main/java/com/example/composecodelabs/MainActivity.kt
+++ app/src/main/java/com/example/composecodelabs/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.composecodelabs
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -26,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composecodelabs.ui.theme.ComposeCodelabsTheme
@@ -66,10 +68,16 @@ fun Greeting(name: String) {
             Column(
                 modifier = Modifier
                     .weight(1f)
+                    //Need to set padding >=0 or app crash
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
 
             ElevatedButton(onClick = { expanded = !expanded }) {
@@ -81,6 +89,19 @@ fun Greeting(name: String) {
 }
 
 //Dp = 320 is the common width of a small phone
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
+@Composable
+fun DefaultPreview() {
+    ComposeCodelabsTheme {
+        Greetings()
+    }
+}
+
 @Preview(showBackground = true, widthDp = 320)
 @Composable
 fun MyAppPreview() {
